7d7e48db763f79fdd420391607f2cb68b4bfee69
student <liamnoob0905@gmail.com>
2018-07-13 14:29:59 -0700

    test

--- main.py
+++ main.py
@@ -4,24 +4,44 @@ import sys
 import platforms
 import random
 from platforms import Platforms
+from player import Player
+
+
+
+
 pygame.init()
 screen_info = pygame.display.Info()
 
 size = (width, height) = (screen_info.current_w, screen_info.current_h)
 screen = pygame.display.set_mode(size)
-clock = pygame.time.clock()
+clock = pygame.time.Clock()
 color = (255,255,179)
 sprite_list = pygame.sprite.Group()
 platforms = pygame.sprite.Group()
+player = ''
 
-def init():
+
+def get_player_actions():
+    p1_actions = {}
+    p1_actions["p1_jump"] = pygame.image.load("image/p1_jump.png").convert()
+    p1_actions["p1_jump"].set_colorkey((0,0,0))
+    p1_actions["p1_hurt"] = pygame.image.load("image/p1_hurt.png").convert()
+    p1_actions["p1_hurt"].set_colorkey((0,0,0))
+    return p1_actions
+
+def init(p1_actions):
+    global player
     for i in range(height // 100):
         for j in range(width // 420):
-            plat = platforms((random.randint(5,0 (width-50) // 10) * 10, 120 * i), 'image/grassHalf.png', 70, 40)
+            plat = Platforms((random.randint(5, (width-50) // 10) * 10, 120 * i), 'image/grassHalf.png', 70, 40)
             platforms.add(plat)
-
+    player = Player((platforms.sprites()[-1].rect.centerx, platforms.sprites()[-1].rect.centery-300), p1_actions)
+    sprite_list.add(player)
 def main():
+    global player, text, text_rect
     game_over = False
+    p1_actions = get_player_actions()
+    init(p1_actions)
     while True:
         clock.tick(60)
         for event in pygame.event.get():
@@ -32,11 +52,16 @@ def main():
                     pygame.display.set_mode(size, FULLSCREEN)
                 if event.key == K_ESCAPE:
                     pygame.display.set_mode(size)
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_LEFT]:
+            player.left()
+        if keys[pygame.K_RIGHT]:
+            player.right()
         screen.fill(color)
         platforms.draw(screen)
         sprite_list.draw(screen)
         pygame.display.flip()
 
 
-if __name__ = '__main__':
+if __name__ == '__main__':
     main()
--- player.py
+++ player.py
@@ -9,16 +9,45 @@ class Player(pygame.sprite.Sprite):
         self.image = images['p1_jump']
         self.rect = self.image.get_rect()
         self.rect.center = pos
-        self.xy_speed = pygame.math.Vector2(0,0)
+        self.xy_speed = pygame.math.Vector2(0, 0)
         self.facing = "R"
-        2
-    def update(self):
+        self.jump_speed = -14
+
+    def update(self, platforms):
+        screen_info = pygame.display.Info()
         self.image = self.images('p1_jump')
         if self.facing == "L":
             self.image = pygame.transform.flip(self.image, True, False)
+        self.rect.move_ip(self.xy_speed)
+        self.xy_speed[0] = 0
+        if self.rect.right < 0:
+            self.rect.left = screen_info.current_w
+        elif self.rect.left > screen_info.current_w:
+            self.rect.right = 0
+        if self.rect.top < 100:
+            self.rect.top = 100
+            for plat in platforms.sprites():
+                plat.scroll(-1 * self.xy_speed[1])
+        elif self.rect.top > screen_info.current_h - 80:
+            self.rect.top = screen_info.current_h - 80
+            for plat in platforms.sprites():
+                if plat.rect.bottom > 0:
+                    plat.scroll(-1 * self.xy_speed[1])
+                else:
+                    plat.kill()
+            return True
+
+        hit_list = pygame.sprite.spritecollide(self, platforms, False)
+        for plat in hit_list:
+            if self.xy_speed[1] > 0 and abs(self.rect.bottom - plat.rect.top) <= self.xy_speed[1]:
+                self.rect.bottom = plat.rect.top
+                self.xy_speed[1] = self.jump_speed
+        self.xyspeed[1] += .5
 
     def left(self):
         self.facing = 'L'
+        self.xy_speed[0] = -6
 
     def right(self):
         self.facing = 'R'
+        self.xy_speed[0] = 6
